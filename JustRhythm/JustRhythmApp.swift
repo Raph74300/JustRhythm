0fc63fb33146cca04f723b20ac3e2db5c384fcd3
Raph74300 <oundjian.r@gmail.com>
2026-07-27 10:53:59 +0200

    Initial Commit

---
+++ JustRhythm/JustRhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustRhythmApp.swift
+//  JustRhythm
+//
+//  Created by Raphaël Oundjian on 27/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct JustRhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
